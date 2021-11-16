8d60770cc07de36b6c922798f1d6eb7e90da0f23
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-11-16 17:29:24 -0500

    Yay, it's alot like C++

---
+++ Tutorial/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string[] stringArray = { "lol", "what", "this", "works", "I", "Guess" };
+            int stringArrayLength = stringArray.Length;
+
+            //output to user
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(stringArrayLength);
+            myPrintFunct(stringArray, stringArrayLength);
+
+        }
+        static void myPrintFunct(string[] stringArray, int stringArrayLength)
+        {
+            //logic
+            for (int i = 0; i < stringArrayLength; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+        }
+    }
+
+
+}
+
